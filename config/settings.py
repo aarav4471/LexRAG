fb96b48f2ae2a5c6a8fd8eac506854206f2200f2
aarav4471 <133221002+aarav4471@users.noreply.github.com>
2026-03-06 00:12:48 +0530

    update settings to support streamlit secrets

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,10 @@
 import os
 from dotenv import load_dotenv
-
+import streamlit as st
 load_dotenv()
 
 class Settings:
-    GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+    GROQ_API_KEY = st.secrets.get("GROQ_API_KEY", os.getenv("GROQ_API_KEY"))
     GROQ_MODEL = os.getenv("GROQ_MODEL", "llama3-70b-8192")
     CHROMA_PATH = os.getenv("CHROMA_PATH")
-    NEO4J_URI = os.getenv("NEO4J_URI")
-    NEO4J_USER = os.getenv("NEO4J_USER")
-    NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+ 
