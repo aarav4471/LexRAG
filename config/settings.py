6f4371d2d3f9a151c610d2d03b4686aa401ce05f
aarav4471 <133221002+aarav4471@users.noreply.github.com>
2026-03-06 00:24:38 +0530

    update groq model

--- config/settings.py
+++ config/settings.py
@@ -5,6 +5,6 @@ load_dotenv()
 
 class Settings:
     GROQ_API_KEY = st.secrets.get("GROQ_API_KEY", os.getenv("GROQ_API_KEY"))
-    GROQ_MODEL = os.getenv("GROQ_MODEL", "llama3-70b-8192")
+    GROQ_MODEL = "llama-3.1-8b-instant"
     CHROMA_PATH = "./chroma_db"
  
